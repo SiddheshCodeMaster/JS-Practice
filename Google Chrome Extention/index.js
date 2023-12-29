bcfe8a3ec4ae1d1841f7d291c8d4d2697e38efab
SiddheshCodeMaster <siddhesh.shankar.codemaster@gmail.com>
2023-12-29 12:42:23 +0530

    Google Chrome Extension Commit 1.0

---
+++ Google Chrome Extention/index.js
@@ -0,0 +1,9 @@
+let myLeads = [];
+
+const input_el = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+
+inputBtn.addEventListener("click",function() {
+    console.log("Button Cliked by Event Listener");
+})
+
