1e92ae757cacca2d6ac433c3b93b77812b0c056e
Siddhesh Shankar <siddhesh.codemaster.github@gmail.com>
2023-12-30 18:54:21 +0530

    Google Chrome Extension Commit 1.0.2

--- Google Chrome Extention/index.js
+++ Google Chrome Extention/index.js
@@ -2,11 +2,19 @@ let myLeads = [];
 
 const input_el = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
-
+const ulEl = document.getElementById("ul-el");
+ 
 inputBtn.addEventListener("click",function() {
     console.log("Button Cliked by Event Listener");
     myLeads.push("www.awesomelead.com");
     myLeads.push(input_el.value)
+    
     console.log(myLeads);
-})
 
+    for(var i=0; i < myLeads.length; i++)
+    {
+        // console.log(myLeads[i]);
+        ulEl.textContent += myLeads[i] + " ";
+    }
+    
+})
